e338d81cb5ccf388c2750745b835108814c564c6
roy <2circumflex@gmail.com>
2024-01-04 15:05:42 +0900

    메타데이터 다루기

--- src/app/[location]/page.tsx
+++ src/app/[location]/page.tsx
@@ -4,12 +4,22 @@ import { getForecast } from "@/utils/getForecast"
 type Props = {
   params: {
     location: string
+  },
+  searchParams: {
+    name: string
   }
 }
 
-export default async function Detail({ params }: Props) {
+export function generateMetadata({ params, searchParams }: Props) {
+  return {
+    title: `날씨 앱 - ${searchParams.name}`,
+    description: `${searchParams.name} 날씨를 알려드립니다`
+  }
+}
+
+export default async function Detail({ params, searchParams }: Props) {
+  const name = searchParams.name
   const res = await getForecast(params.location)
-  const name = res.location.name
 
   return (
     <>
